0ccc1d3cbc7c738ddd9a584cf7b88638f357a83b
SSSSShark <55078844+SSSSShark@users.noreply.github.com>
2021-07-11 11:54:27 +0800

    拔河胜利预留

--- Assets/Script/TugOfWarSystem.cs
+++ Assets/Script/TugOfWarSystem.cs
@@ -7,23 +7,32 @@ public class TugOfWarSystem : MonoBehaviour
     [Header("胜利阈值")]
     [SerializeField] int maxTug = 10;
     [Header("最长时长")]
-    [SerializeField] int time = 5;
+    [SerializeField] float time = 5;
     [Space(30)]
     [SerializeField] GameObject tugUI;
     [SerializeField] Communication communicationSO;
     [SerializeField] private int tugging;
     private float timeAcc;
+    private bool showFlag;
+    [SerializeField] private float showTime = 1.0f;
+    private float showTimeAcc;
+
+    [SerializeField] GameObject Awin;
+    [SerializeField] GameObject Bwin;
+
 
     // Start is called before the first frame update
     void Start()
     {
         tugUI.SetActive(false);
+        Awin.SetActive(false);
+        Bwin.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (communicationSO.GMToTugSystem)
+        if (communicationSO.GMToTugSystem&&!showFlag)
         {
             if (!tugUI.activeSelf)
             {
@@ -55,10 +64,24 @@ public class TugOfWarSystem : MonoBehaviour
             if (timeAcc >= time || tugging >= 10 || tugging <= -10)
             {
                 communicationSO.tugResult = (tugging > 0) ? 1 : ((tugging < 0) ? -1 : 0);
+                showTimeAcc = 0;
+                showFlag = true;
+                Awin.SetActive(communicationSO.tugResult > 0);
+                Bwin.SetActive(communicationSO.tugResult < 0);
+            }
+        }
+        else if (showFlag)
+        {
+            showTimeAcc += Time.deltaTime;
+            if (showTimeAcc >= showTime)
+            {
                 communicationSO.tugSystemToGM = true;
                 communicationSO.tugOfWar = false;
                 communicationSO.GMToTugSystem = false;
+                Awin.SetActive(false);
+                Bwin.SetActive(false);
                 tugUI.SetActive(false);
+                showFlag = false;
             }
         }
     }
