12d24d6a95d2746161c83519a19b9819542b4392
SSSSShark <55078844+SSSSShark@users.noreply.github.com>
2021-07-11 15:17:19 +0800

    开局UI流程

--- Assets/Script/ArrowManager.cs
+++ Assets/Script/ArrowManager.cs
@@ -8,6 +8,7 @@ public class ArrowManager : MonoBehaviour
     [SerializeField] GameObject up;
     [SerializeField] GameObject left;
     [SerializeField] GameObject right;
+    [SerializeField] GameObject first;
     [SerializeField] bool player;
     EnergySystem ES;
 
@@ -20,18 +21,19 @@ public class ArrowManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        up.SetActive(communicationSO.availableDirect[0]);
-        left.SetActive(communicationSO.availableDirect[2]);
-        right.SetActive(communicationSO.availableDirect[3]);
+        up.SetActive(communicationSO.availableDirect[0] && !communicationSO.isFirstEnergy);
+        left.SetActive(communicationSO.availableDirect[2] && !communicationSO.isFirstEnergy);
+        right.SetActive(communicationSO.availableDirect[3]&&!communicationSO.isFirstEnergy);
+        first.SetActive(communicationSO.isFirstEnergy);
         if (ES.showFlag)
         {
-            if (player && communicationSO.result > 0)
+            if (player && communicationSO.result > 0 && !communicationSO.isFirstEnergy)
             {
                 up.SetActive(communicationSO.playerADirect == 0);
                 left.SetActive(communicationSO.playerADirect == 2);
                 right.SetActive(communicationSO.playerADirect == 3);
             }
-            else if (!player && communicationSO.result < 0)
+            else if (!player && communicationSO.result < 0 && !communicationSO.isFirstEnergy)
             {
                 up.SetActive(communicationSO.playerBDirect == 0);
                 left.SetActive(communicationSO.playerBDirect == 2);
